4a5bdda54b6bddcfa5cd60af571c8bfed7e83c04
thanadon <55877951+HELLOMARK1@users.noreply.github.com>
2025-05-23 10:01:46 +0700

    Add files via upload

--- index.ts
+++ index.ts
@@ -7,11 +7,11 @@ dotenv.config()
 
 const app = new Elysia()
   .use(cors({
-    origin: true, // หรือใส่ origin เฉพาะเช่น: 'http://localhost:4321'
+    origin: true,
     credentials: true
   }))
   .use(authRoutes)
-  .get('/', () => 'API is live!')
+  .get('/', () => '✅ API พร้อมใช้งานแล้ว (ระบบหลังบ้านเชื่อมต่อสำเร็จ)')
   .listen(3000)
 
 console.log('✅ API running at http://localhost:3000')
